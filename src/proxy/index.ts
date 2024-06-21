0c1e21d9a4c93df9c157177143870d9e8ae60b92
j4ndrw <murjan_andreialin@yahoo.com>
2024-06-21 22:18:14 +0300

    another kerfuffle with system prompts

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -7,6 +7,8 @@ import { zValidator } from "@hono/zod-validator";
 const MAX_MESSAGES = 100;
 let messages: Message[] = [];
 
+const systemPrompts = new Map<string, string>();
+
 const pushMessage = (message: Message) => {
   if (messages.length > MAX_MESSAGES) return;
   messages.push(message);
@@ -17,7 +19,14 @@ const app = new Hono();
 const llm = new Ollama({ host: "http://localhost:11434" });
 const model = "llama3";
 const systemPrompt = (user: string) =>
-  `You are a Minecraft expert, responsible with helping players on the server with whatever they need, as well as help them have a good time. For reference, the user interacting with you directly is called '${user}'.`;
+  `
+You are a Minecraft expert, responsible with helping players on the server
+with whatever they need, as well as help them have a good time.
+
+For reference, the user interacting with you directly is called '${user}'.
+
+Try to answer in using 3 paragraphs maximum and make them concise.
+`;
 
 const schema = z.object({
   question: z.string(),
@@ -30,12 +39,15 @@ app.get("/", zValidator("query", schema), async (c) => {
   const { question, user } = c.req.valid("query");
   const decodedQuestion = Buffer.from(question, "base64").toString();
 
-  pushMessage({ role: "system", content: systemPrompt(user) });
+  if (!systemPrompts.has(user)) systemPrompts.set(user, systemPrompt(user));
   pushMessage({ role: "user", content: decodedQuestion });
 
   const { message } = await llm.chat({
     model,
-    messages,
+    messages: [
+      { role: "system", content: systemPrompts.get(user)! },
+      ...messages,
+    ],
   });
 
   pushMessage(message);
